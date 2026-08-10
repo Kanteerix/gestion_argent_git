d4ba8565aab21f068b3e817c91d498dbde8fb84a
Kanto RAKOTONDRASOA <rakokanto007@gmail.com>
2026-08-10 12:29:20 +0300

    summ

--- sql/money.sql
+++ sql/money.sql
@@ -8,6 +8,7 @@ CREATE TABLE retrait (
     retrait_id INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
     the_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL, 
     personne_id INT,
+    raison_titre VARCHAR(100),
     raisons_detaille_id INT,  
     reste INT  
 );
@@ -16,4 +17,10 @@ CREATE TABLE raisons_detaille (
     raisons TEXT,
     prix INT    
 );
+INSERT INTO personnes (personne_name) VALUES 
+("Princia"),
+("Dada"),
+("Neny"),
+("Kevin"),
+("Anja");
 
