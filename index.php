52f31e5f37564ef16155c0cd617f57021c73773b
Kanto RAKOTONDRASOA <rakokanto007@gmail.com>
2026-08-10 12:29:30 +0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>CHOIX</title>
+</head>
+<body>
+    <head>
+
+    </head>
+    <main>
+        <a href="pages/retrait.php"><button>RETRAIT</button></a> OU <a href="pages/depot.php"><button>DEPOT</button></a>
+    </main>
+    <footer>
+
+    </footer>
+</body>
+</html>
