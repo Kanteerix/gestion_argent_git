415c5dd0cc5f0a4bf5bf8e5c8eca1056f3924173
Kanto RAKOTONDRASOA <rakokanto007@gmail.com>
2026-08-19 12:07:50 +0300

    ok

--- pages/depot_retrait.php
+++ pages/depot_retrait.php
@@ -28,29 +28,41 @@ $value_types = get_enum_values($nom_table, $nom_column) ;
         <?php }?>
         <div class="t-card">
             <form action="../php/traitements/traitement_transaction.php" method="get">
-                <label for="personne">Personne : </label>
-                <select name="personne_id" id="personne">
-                    <?php 
-                for ($i=0; $i <= count($all_personnes)-1 ; $i++) { ?>
-                    <option value="<?= $all_personnes[$i]['personne_id'] ?>" 
-                    <?= $all_personnes[$i]['personne_name']=="Dada" ? "selected" : "" ?>>
-                    <?= $all_personnes[$i]['personne_name'] ?>
-                </option>
-                <?php } ?>
-                </select><br>
-                <label for="choix">Choix : </label>
-                <select name="choix" id="choix">
-                    <?php 
-                        foreach ($value_types as $v_t) { ?>
-                        <option value="<?= $v_t ?>"><?= $v_t ?></option>
-                        <?php }
-                        ?>
-                </select><br>
-                <label for="raison">Titre RAISONS : </label>
-                <input type="text" name="raison" id="raison"><br>
-                <label for="montant">Montant : </label>
-                <input type="number" name="montant" id="montant"><br>
-                <input type="submit" value="Deposer">
+                <div class="champ">
+                    <label for="personne">Personne : </label>
+                    <select name="personne_id" id="personne">
+                        <?php 
+                        for ($i=0; $i <= count($all_personnes)-1 ; $i++) { ?>
+                        <option value="<?= $all_personnes[$i]['personne_id'] ?>" 
+                        <?= $all_personnes[$i]['personne_name']=="Dada" ? "selected" : "" ?>>
+                        <?= $all_personnes[$i]['personne_name'] ?>
+                        </option>
+                        <?php } ?>
+                    </select>
+                </div>
+                <div class="champ">
+                    <label for="choix">Choix : </label>
+                    <select name="choix" id="choix">
+                        <?php 
+                            foreach ($value_types as $v_t) { ?>
+                            <option value="<?= $v_t ?>"
+                            <?= $v_t=="retrait" ? "selected" : ""?>>
+                            <?= $v_t ?></option>
+                            <?php }
+                            ?>
+                    </select>
+                </div>
+                <div class="champ">
+                    <label for="raison">Titre RAISONS : </label>
+                    <input type="text" name="raison" id="raison">
+                </div>
+                <div class="champ">
+                    <label for="montant">Montant : </label>
+                    <input type="number" name="montant" id="montant">
+                </div>
+                <div class="champ">
+                    <input type="submit" value="Deposer">
+                </div>
             </form>
         </div>
     </main>
--- sql/money.sql
+++ sql/money.sql
@@ -4,23 +4,6 @@ CREATE TABLE personnes (
     personne_id INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
     personne_name VARCHAR(100) NOT NULL
 );
-CREATE TABLE historique_depot (
-    historique_depot_id INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
-    the_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
-    personne_id INT,
-    raison_titre VARCHAR(100),
-    montant INT, 
-    total INT  
-);
-CREATE TABLE historique_retrait (
-    historique_retrait_id INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
-    the_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL, 
-    personne_id INT,
-    raison_titre VARCHAR(100),
-    raisons_detaille_id INT, 
-    total_retrait INT, 
-    reste INT  
-);
 CREATE TABLE historique_depot_retrait (
     historique_depot_retrait_id INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
     the_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL, 
@@ -30,11 +13,6 @@ CREATE TABLE historique_depot_retrait (
     solde_initial INT,
     montant INT,
     solde_final INT
-);
-CREATE TABLE raisons_detaille (
-    raisons_detaille_id INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
-    raisons TEXT,
-    prix INT    
 );
 INSERT INTO personnes (personne_name) VALUES 
 ("Princia"),
