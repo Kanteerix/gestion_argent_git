b1e490f26bb904cdda97aa3177a3c361f7453343
Kanto RAKOTONDRASOA <rakokanto007@gmail.com>
2026-09-05 09:39:45 +0300

    Create random_laoka.php

---
+++ pages/random_laoka.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Random LAOKA</title>
+</head>
+<body>
+    <head>
+
+    </head>
+    <main>
+        <a href="add_laoka.php"><button>Add LAOKA   </button></a>
+        <a href=""><button>All</button></a>
+        <a href=""><button>Poisson</button></a>
+        <a href=""><button>Viande Boeuf</button></a>
+        <a href=""><button>Viande Porc</button></a>
+        <a href=""><button>Viande Poulet</button></a>
+        <a href=""><button>Vegetarien</button></a>
+    </main>
+    <footer>
+
+    </footer>
+</body>
+</html>
