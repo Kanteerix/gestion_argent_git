60e7227eb7b88b38a64a58e98b51e84e2cd68215
Kanto RAKOTONDRASOA <rakokanto007@gmail.com>
2026-09-05 16:22:55 +0300

    Update random_laoka.php

--- pages/random_laoka.php
+++ pages/random_laoka.php
@@ -7,16 +7,20 @@
 </head>
 <body>
     <head>
-
+        <a href="../index.php">Retour</a>
     </head>
     <main>
         <a href="add_laoka.php"><button>Add LAOKA   </button></a>
         <a href=""><button>All</button></a>
         <a href=""><button>Poisson</button></a>
-        <a href=""><button>Viande Boeuf</button></a>
-        <a href=""><button>Viande Porc</button></a>
-        <a href=""><button>Viande Poulet</button></a>
+        <a href=""><button>Boeuf</button></a>
+        <a href=""><button>Boeuf Hache</button></a>
+        <a href=""><button>Porc</button></a>
+        <a href=""><button>Volailles</button></a>
         <a href=""><button>Vegetarien</button></a>
+        <a href=""><button>Saucisse</button></a>
+        <a href=""><button>Lasary</button></a>
+        <a href=""><button>Dessert</button></a>
     </main>
     <footer>
 
