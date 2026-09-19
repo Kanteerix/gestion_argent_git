24e8b26fab6cc31eb18f0330bca44561ac31844e
Kanto RAKOTONDRASOA <rakokanto007@gmail.com>
2026-09-19 05:52:27 +0300

    Create categories_ToDo.php

---
+++ pages/categories_ToDo.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Categories ToDo</title>
+</head>
+<body>
+    <header>
+
+    </header>
+    <main>
+        <h1>Choisir une categorie</h1>
+        <a href="form_ToDo.php?nature=categorie"><button>+ Ajouter un categorie</button></a>
+    </main>
+    <footer>
+
+    </footer>
+</body>
+</html>
